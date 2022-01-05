8444717c1574fc0760c778a7fca73d9dffeffca1
Rahul N Shettigar <92166609+Rahs0601@users.noreply.github.com>
2022-01-05 18:41:52 +0530

    Add files via upload

--- python/Practice/Bill spliter.py
+++ python/Practice/Bill spliter.py
@@ -1,8 +1,7 @@
 def sort_dict_by_value(d, reverse=False):
     return dict(sorted(d.items(), key=lambda x: x[1], reverse=reverse))
-
-
-num = int(input("Enter the number of person "))
+print("\n \t WelCome To Bill spliter Made By Rah'S\t \n")
+num = int(input("Enter the number of person \n "))
 peps = {}
 for _ in range(num):
     name = input("enter the names ")
@@ -12,18 +11,30 @@ avg = sum(peps.values()) / num
 print("average = ", avg)
 highest_payer = {k: v - avg for (k, v) in peps.items() if v > avg}
 sort_dict_by_value(highest_payer)
-print("highest = ", highest_payer)
-lowest_payer = {k: v - avg for (k, v) in peps.items() if v < avg}
+print("People who should get", highest_payer)
+lowest_payer = {k: avg -v for (k, v) in peps.items() if v < avg}
 sort_dict_by_value(lowest_payer)
-print("lowest= ", lowest_payer)
-for (k, v) in highest_payer.items():
-    for (a, b) in lowest_payer.items():
-        if v + b > 0:
-            print(f"{a} has to pay {-b}rs to {k}")
-            if avg + b == 0:
-                lowest_payer[a.title()] = 0
-                highest_payer[k.title()] = v+b
-                print(highest_payer, lowest_payer)
-        else:
-            print(f"{a} has to pay {v + b}rs to {k}")
-    print(highest_payer,lowest_payer)
+print("People eho should give ", lowest_payer)
+for (k,v ) in highest_payer.items():
+    temp =v
+    for(a,b) in lowest_payer.items(): 
+        temp2 =b    
+        while (temp!=0 and  b!=0):
+            if temp-temp2 == 0:
+                print(f"{a} has to pay {temp2}rs to {k}")
+                temp=0 
+                temp2=0
+                break      
+            elif temp-temp2 <0:
+                print(f"{a} has to pay {-temp+temp2}rs to {k}")
+                temp2=temp2-temp
+                temp=0
+                break      
+            else:
+                print(f"{a} has to pay {temp2}rs to {k}")
+                temp=temp-temp2
+                temp2=0     
+                break
+        highest_payer[k]=temp
+        lowest_payer[a]=temp2
+        
